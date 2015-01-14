1c3e9420a33d8b361832fc491865dd0feff8297a
William <william.campolina@gmail.com>
2015-01-13 23:29:24 -0200

    closure funfando

--- util.js
+++ util.js
@@ -1,61 +1,35 @@
-$(document).ready(function(){
-
-	/*function vaibuscarasissues(){
-		console.log("teste");
+function vaibuscarasissues(){
     function GithubRepo(username, reponame){
-        //seu codigo aqui...
+    	var tabela;
+        var busca = {
+					'busca_issues': function(){
+						var url = "https://api.github.com/repos/"+username+"/"+reponame+"/issues";
+						return $.get(url).success(function(result){
+								tabela = result;
+						});
+				},
+					'popula_tabela': function(issuestable){
+						console.log("2222");
+							for (var i = 0; i < tabela.length; i++) {
+							$("#issuestable").append(
+												"<tr>" +
+												"    <th>" + tabela[i]["number"] + "</th>" +
+												"    <th>" + tabela[i]["title"] + "</th>" +
+												"</tr>"
+												);
+						};
+				},
+
+        }
+        return busca;
+
     }
 
-    //mais um pouco de codigo aqui....
 
+    var username = $("#user").val();
+	var reponame = $("#reponame").val();
     var le_repo = GithubRepo(username, reponame);
-    le_repo.busca_issues();
-    le_repo.popula_tabela("#issuestable");
-}*/
-
-
-
-
-
-
-	$("#vaibuscarasissues").click(function() {
-		console.log("teste");
-		var username = $("#user").val();
-		var reponame = $("#reponame").val();
-
-		function GithubRepo(username, reponame){
-        //https://api.github.com/repos/freedomsponsors/www.freedomsponsors.org/issues
-        var url = "https://api.github.com/repos/"+username+"/"+reponame+"/issues";
-	        console.log(url);
-	       $.get(url).success(function(result){
-				
-				console.log(result[0]["number"]);
-				console.log("terminou");
-
-				for (var i = 0; i < result.length; i++) {
-					/*console.log(result[i]["number"]);
-					console.log(result[i]["title"]);*/
-
-					$("#issuestable").append(
-                            "<tr>" +
-                            "    <th>" + result[i]["number"] + "</th>" +
-                            "    <th>" + result[i]["title"] + "</th>" +
-                            "</tr>"
-                            );
-				};
-			});
-		}
-
-		var le_repo = GithubRepo(username, reponame);
-		//le_repo.busca_issues();
-		//le_repo.popula_tabela("#issuestable");
-
-
-	});
-
-
-
-
-
-	
-});
+    le_repo.busca_issues().success(function(){
+        le_repo.popula_tabela("#issuestable");
+    });
+}
